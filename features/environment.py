8800eb3b9e18c997c686083bf4ec5d178abee29b
hborcher <hborcher@illinois.edu>
2018-02-02 15:53:01 -0600

    create and clean up with each scenerio

--- features/environment.py
+++ features/environment.py
@@ -1,11 +1,9 @@
 import tempfile
-temp_dir = None
 
 def before_scenario(context, scenario):
-    global temp_dir
-    temp_dir = tempfile.TemporaryDirectory()
-    context.temp_dir = temp_dir.name
+
+    context._temp_dir = tempfile.TemporaryDirectory()
+    context.temp_dir = context._temp_dir.name
 
 def after_scenario(context, scenario):
-    global temp_dir
-    temp_dir.cleanup()
+    context._temp_dir.cleanup()
