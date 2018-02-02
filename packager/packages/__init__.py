c8d3090579d1f33151b1d090d4c0e492cb1b03a9
hborcher <hborcher@illinois.edu>
2018-02-02 15:51:56 -0600

    removed commented out code

--- packager/packages/__init__.py
+++ packager/packages/__init__.py
@@ -1,5 +1,2 @@
-# from .utils import create_package
-from .hathi_lab import HathiLabPackage
-from .capture_one import CaptureOnePackage
-# from .hathi_submit import HathiSubmitPackage as HathiSubmit
-# from .exceptions import PackageError, PackagePartMissing
+from .capture_one_package import CaptureOnePackage
+from .hathi_tiff_package import HathiTiff
