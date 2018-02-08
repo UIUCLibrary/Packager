c78bcf65eedf6f6faf3e128cb153dcb6927423c6
hborcher <hborcher@illinois.edu>
2018-02-08 13:12:23 -0600

    PSR-78 Convert CaptureOne Preservation TIFF to Digital Library Compound Objects

--- tests/test_CapOne.py
+++ tests/test_CapOne.py
@@ -65,3 +65,46 @@ def test_capture_one_tiff_package_size(capture_one_fixture):
     capture_one_packages = list(capture_one_packages_factory.locate_packages(path=source))
     # There should be 2 packages in this sample batch
     assert len(capture_one_packages) == 2
+
+
+def test_capture_one_tiff_to_digital_library(capture_one_fixture):
+    source = os.path.join(capture_one_fixture, CAPTURE_ONE_BATCH_NAME)
+    dest = os.path.join(capture_one_fixture, DESTINATION_NAME)
+
+
+    capture_one_packages_factory = packager.PackageFactory(packager.packages.CaptureOnePackage())
+    # find all Capture One organized packages
+    capture_one_packages = list(capture_one_packages_factory.locate_packages(path=source))
+    # There should be 2 packages in this sample batch
+    assert len(capture_one_packages) == 2
+
+    dl_factory = packager.PackageFactory(packager.packages.DigitalLibraryCompound())
+    for capture_one_package in capture_one_packages:
+        dl_factory.transform(capture_one_package, dest=dest)
+
+    # This should result in the following files
+    #
+    #  some_root/000001/preservation/00000001.tif
+    assert os.path.exists(os.path.join(dest, "000001", 'preservation', "000001_00000001.tif"))
+    #  some_root/000001/access/
+    assert os.path.exists(os.path.join(dest, "000001", 'access'))
+
+    #  some_root/000001/preservation/00000002.tif
+    assert os.path.exists(os.path.join(dest, "000001", 'preservation', "000001_00000002.tif"))
+    #  some_root/000001/access/
+    assert os.path.exists(os.path.join(dest, "000001", 'access'))
+
+    #  some_root/000001/preservation/00000003.tif
+    assert os.path.exists(os.path.join(dest, "000001", 'preservation', "000001_00000003.tif"))
+    #  some_root/000001/access/
+    assert os.path.exists(os.path.join(dest, "000001", 'access'))
+
+    #  some_root/000002/preservation/00000001.tif
+    assert os.path.exists(os.path.join(dest, "000002", "preservation", "000002_00000001.tif"))
+    #  some_root/000002/access/
+    assert os.path.exists(os.path.join(dest, "000002", "access"))
+
+    #  some_root/000002/preservation/00000001.tif
+    assert os.path.exists(os.path.join(dest, "000002", "preservation", "000002_00000001.tif"))
+    #  some_root/000002/access/
+    assert os.path.exists(os.path.join(dest, "000002", "access"))
