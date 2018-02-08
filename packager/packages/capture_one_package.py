33c687e16705c9edafc647db3e9591ed153b2839
hborcher <hborcher@illinois.edu>
2018-02-08 11:11:07 -0600

    implemented transform method

--- packager/packages/capture_one_package.py
+++ packager/packages/capture_one_package.py
@@ -1,3 +1,6 @@
+import logging
+import os
+import shutil
 import typing
 from . import collection_builder
 from packager.packages.collection import Package
@@ -11,4 +14,17 @@ class CaptureOnePackage(AbsPackageBuilder):
             yield package
 
     def transform(self, package: Package, dest: str) -> None:
-        pass
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+
+        for item in package:
+            item_name = item.metadata['item_name']
+            object_name = item.metadata['id']
+            for inst in item:
+                assert len(inst.files) == 1
+                for file_ in inst.files:
+                    _, ext = os.path.splitext(file_)
+                    new_file_name = "{}_{}{}".format(object_name, item_name, ext)
+                    new_file_path = os.path.join(dest, new_file_name)
+                    logger.info("Copying {} to {}".format(file_, new_file_path ))
+                    shutil.copy(file_, new_file_path)
