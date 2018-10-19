fc44bcc24c1386022147a67817ce99e3a8974ed3
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-10-19 15:53:01 -0500

    require py3exiv2bind at least 0.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     namespace_packages=["uiucprescon"],
     test_suite="tests",
     setup_requires=['pytest-runner'],
-    install_requires=["py3exiv2bind>=0.1.1"],
+    install_requires=["py3exiv2bind>=0.1.2"],
     extras_require={"kdu": ['pykdu-compress>=0.1.0']},
     tests_require=['pytest'],
     zip_safe=False,
