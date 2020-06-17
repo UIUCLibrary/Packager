83ea6ae7ecd2c7cbc9410d9ef4c8a07e52bba415
henry <henryborchers@yahoo.com>
2020-06-17 15:57:20 -0500

    split transform method into transform_one

--- uiucprescon/packager/packages/hathi_jp2_package.py
+++ uiucprescon/packager/packages/hathi_jp2_package.py
@@ -23,36 +23,41 @@ class HathiJp2(AbsPackageBuilder):
         logger.setLevel(AbsPackageBuilder.log_level)
 
         for item in package:
-            item_name = item.metadata[Metadata.ITEM_NAME]
-            object_name = item.metadata[Metadata.ID]
-            new_item_path = os.path.join(dest, object_name)
-            if not os.path.exists(new_item_path):
-                os.makedirs(new_item_path)
+            self.transform_one(item, dest, logger)
 
-            for inst in item:
-                if len(inst.files) != 1:
-                    raise AssertionError(
-                        f"Expected 1 file, found {len(inst.files)}")
+    @staticmethod
+    def transform_one(item, dest, logger=None):
+        item_name = item.metadata[Metadata.ITEM_NAME]
+        object_name = item.metadata[Metadata.ID]
+        new_item_path = os.path.join(dest, object_name)
 
-                for file_ in inst.files:
-                    _, ext = os.path.splitext(file_)
+        if not os.path.exists(new_item_path):
+            os.makedirs(new_item_path)
 
-                    new_file_name = item_name + ext
-                    new_file_path = os.path.join(new_item_path, new_file_name)
+        for inst in item:
+            if len(inst.files) != 1:
+                raise AssertionError(
+                    f"Expected 1 file, found {len(inst.files)}")
 
-                    if ext.lower() == ".jp2":
+            for file_ in inst.files:
+                _, ext = os.path.splitext(file_)
 
-                        # If the item is already a jp2 then copy
-                        file_transformer = transformations.Transformers(
-                            strategy=transformations.CopyFile(),
-                            logger=logger
-                        )
-                    else:
-                        # If it's not the same extension, convert it to jp2
-                        file_transformer = transformations.Transformers(
-                            strategy=transformations.ConvertJp2Hathi(),
-                            logger=logger
-                        )
+                new_file_name = item_name + ext
+                new_file_path = os.path.join(new_item_path, new_file_name)
 
-                    file_transformer.transform(source=file_,
-                                               destination=new_file_path)
+                if ext.lower() == ".jp2":
+
+                    # If the item is already a jp2 then copy
+                    file_transformer = transformations.Transformers(
+                        strategy=transformations.CopyFile(),
+                        logger=logger
+                    )
+                else:
+                    # If it's not the same extension, convert it to jp2
+                    file_transformer = transformations.Transformers(
+                        strategy=transformations.ConvertJp2Hathi(),
+                        logger=logger
+                    )
+
+                file_transformer.transform(source=file_,
+                                           destination=new_file_path)
