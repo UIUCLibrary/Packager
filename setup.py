1212fc9f763c8d9b9967d5d7e07ff13153ff883e
henry <henryborchers@yahoo.com>
2020-06-25 18:09:35 -0500

    PSR-182 unify python packages installed on CI

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     test_suite="tests",
     setup_requires=['pytest-runner'],
     install_requires=["py3exiv2bind>=0.1.5b3"],
-    extras_require={"kdu": ['pykdu-compress>=0.1.3b1']},
+    extras_require={"kdu": ['pykdu-compress>=0.1.3b2']},
     tests_require=['pytest'],
     zip_safe=False,
     python_requires='>=3.5',
