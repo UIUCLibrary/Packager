ce1cbc7501e0615d949471bbda0313575712ebf9
henry <henryborchers@yahoo.com>
2021-03-12 10:50:47 -0600

    remove debug

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     namespace_packages=["uiucprescon"],
     test_suite="tests",
     setup_requires=['pytest-runner'],
-    install_requires=["py3exiv2bind>=0.1.5"],
+    install_requires=['py3exiv2bind>=0.1.5'],
     extras_require={"kdu": ['pykdu-compress>=0.1.3']},
     tests_require=['pytest', 'pytest-bdd<4.0'],
     zip_safe=False,
