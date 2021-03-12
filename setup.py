1562c7f53893feb8f3c17379dd891d98ae3dcb0e
henry <henryborchers@yahoo.com>
2021-03-12 10:36:14 -0600

    tests_require pytest bdd < 4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     setup_requires=['pytest-runner'],
     install_requires=["py3exiv2bind>=0.1.5"],
     extras_require={"kdu": ['pykdu-compress>=0.1.3']},
-    tests_require=['pytest'],
+    tests_require=['pytest', 'pytest-bdd<4.0'],
     zip_safe=False,
     python_requires='>=3.5',
 )
