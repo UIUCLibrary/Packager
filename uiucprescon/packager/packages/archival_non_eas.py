a6748d6e2bde5c994c11002521429fff066e1195
henry <henryborchers@yahoo.com>
2021-04-03 14:52:57 -0500

    init commit

---
+++ uiucprescon/packager/packages/archival_non_eas.py
@@ -0,0 +1,15 @@
+import typing
+
+from .abs_package_builder import AbsPackageBuilder
+from .collection import Package
+from . import collection_builder
+
+
+class ArchivalNonEAS(AbsPackageBuilder):
+
+    def locate_packages(self, path: str) -> typing.Iterator[Package]:
+        package_builder = collection_builder.ArchivalNonEASBuilder()
+        yield from package_builder.build_batch(path)
+
+    def transform(self, package: Package, dest: str) -> None:
+        pass
