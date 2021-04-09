54ce68a417de2950965130620492c16821dcc4d8
henry <henryborchers@yahoo.com>
2021-04-09 17:42:24 -0500

    next iteration

--- uiucprescon/packager/packages/__init__.py
+++ uiucprescon/packager/packages/__init__.py
@@ -5,11 +5,14 @@ from .hathi_tiff_package import HathiTiff
 from .digital_library_compound import DigitalLibraryCompound
 from .hathi_jp2_package import HathiJp2
 from .hathi_limited_package import HathiLimitedView
+from .noneas import CatalogedNonEAS, ArchivalNonEAS
 
 __all__ = [
     "CaptureOnePackage",
     "HathiTiff",
     "DigitalLibraryCompound",
     "HathiJp2",
-    "HathiLimitedView"
+    "HathiLimitedView",
+    "CatalogedNonEAS",
+    "ArchivalNonEAS"
 ]
