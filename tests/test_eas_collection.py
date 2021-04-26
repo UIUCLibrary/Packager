9b4bb2539908f53a21cc44d3c7f22e9b17edd13c
henry <henryborchers@yahoo.com>
2021-04-26 15:09:14 -0500

    add eas package type

---
+++ tests/test_eas_collection.py
@@ -0,0 +1,262 @@
+import os.path
+import pathlib
+from unittest.mock import Mock, call, ANY
+
+import pytest
+
+from uiucprescon import packager
+from uiucprescon.packager.packages import eas, DigitalLibraryCompound, HathiJp2
+from uiucprescon.packager.packages.collection import Batch
+
+
+class TestEASCollection:
+    @pytest.fixture()
+    def sample_access_files(self):
+        access_files = [
+            "99338384012205899-00000001.tif",
+            "99338384012205899-00000002.tif",
+            "99350592312205899-00000001.tif",
+            "99350592312205899-00000002.tif",
+        ]
+        return access_files
+
+    @pytest.fixture()
+    def sample_collection_path(self, sample_access_files, monkeypatch):
+        batch_dir = "batch1"
+
+        valid_paths = [
+            batch_dir,
+            os.path.join(batch_dir, "access")
+        ]
+
+        monkeypatch.setattr(
+            packager.packages.eas.os.path,
+            "exists", lambda path: path in valid_paths
+        )
+
+        monkeypatch.setattr(os, "makedirs", Mock())
+
+        for f in sample_access_files:
+            valid_paths.append(os.path.join(batch_dir, "access", f))
+
+        def scandir(path):
+            for f in sample_access_files:
+                new_file = Mock(
+                    path=os.path.join(path, f),
+                )
+                new_file.name = f
+                yield new_file
+
+        monkeypatch.setattr(eas.os, "scandir", scandir)
+
+        def locate_package_files(_, path: str):
+            for f in sample_access_files:
+                yield pathlib.Path(os.path.join(path, f))
+
+        monkeypatch.setattr(
+            eas.EASBuilder,
+            "locate_package_files",
+            locate_package_files
+        )
+
+        return batch_dir
+
+    def test_locate(self, sample_collection_path):
+        factory = packager.PackageFactory(eas.Eas())
+        packages = list(factory.locate_packages(sample_collection_path))
+        assert len(packages) == 2
+
+    @pytest.mark.parametrize("expected_source, expected_destination", [
+        (
+            "batch1/access/99338384012205899-00000001.tif",
+            "out/99338384012205899/00000001.jp2"
+        ),
+        (
+            "batch1/access/99338384012205899-00000002.tif",
+            "out/99338384012205899/00000002.jp2"
+        ),
+        (
+            "batch1/access/99350592312205899-00000001.tif",
+            "out/99350592312205899/00000001.jp2"
+        ),
+        (
+            "batch1/access/99350592312205899-00000002.tif",
+            "out/99350592312205899/00000002.jp2"
+        )
+    ])
+    def test_transform_to_hathi(self, sample_collection_path, expected_source, expected_destination, monkeypatch):
+        factory = packager.PackageFactory(eas.Eas())
+        packages = factory.locate_packages(sample_collection_path)
+        destination_type = packager.PackageFactory(HathiJp2())
+        output = "out"
+
+        transform = Mock()
+        monkeypatch.setattr(packager.packages.hathi_jp2_package.transformations.Transformers, "transform", transform)
+
+        for p in packages:
+
+            destination_type.transform(p, output)
+            assert transform.called is True
+        transform.assert_has_calls(
+            [
+                call(source=expected_source, destination=expected_destination)
+            ]
+        )
+
+    def test_transform_to_dl(self, sample_collection_path, monkeypatch):
+        factory = packager.PackageFactory(eas.Eas())
+        packages = factory.locate_packages(sample_collection_path)
+        destination_type = packager.PackageFactory(DigitalLibraryCompound())
+        output = "out"
+
+        transform = Mock()
+        with monkeypatch.context() as mp:
+
+            mp.setattr(
+                packager.transformations.Transformers,
+                "transform",
+                transform
+            )
+
+            for p in packages:
+                destination_type.transform(p, output)
+                assert transform.called is True
+
+        transform.assert_has_calls(
+            [
+                call(
+                    os.path.join(sample_collection_path, "access", "99338384012205899-00000001.tif"),
+                    os.path.join(output, "99338384012205899", "access", "99338384012205899-00000001.jp2")
+                ),
+                call(
+                    source=os.path.join(sample_collection_path, "access",
+                                        "99338384012205899-00000001.tif"),
+                    destination=os.path.join(output,
+                                             "99338384012205899",
+                                             "preservation",
+                                             "99338384012205899-00000001.tif")
+                ),
+                call(
+                    os.path.join(sample_collection_path, "access", "99338384012205899-00000002.tif"),
+                    os.path.join(output, "99338384012205899", "access", "99338384012205899-00000002.jp2")
+                ),
+                call(
+                    source=os.path.join(sample_collection_path, "access",
+                                        "99338384012205899-00000002.tif"),
+                    destination=os.path.join(output,
+                                             "99338384012205899",
+                                             "preservation",
+                                             "99338384012205899-00000002.tif")
+                ),
+                call(
+                    os.path.join(sample_collection_path, "access", "99350592312205899-00000001.tif"),
+                    os.path.join(output, "99350592312205899", "access", "99350592312205899-00000001.jp2")
+                ),
+                call(
+                    source=os.path.join(sample_collection_path, "access",
+                                        "99350592312205899-00000001.tif"),
+                    destination=os.path.join(output,
+                                             "99350592312205899",
+                                             "preservation",
+                                             "99350592312205899-00000001.tif")
+                ),
+                call(
+                    os.path.join(sample_collection_path, "access", "99350592312205899-00000002.tif"),
+                    os.path.join(output, "99350592312205899", "access", "99350592312205899-00000002.jp2")),
+                call(
+                    source=os.path.join(sample_collection_path, "access",
+                                        "99350592312205899-00000002.tif"),
+                    destination=os.path.join(output,
+                                             "99350592312205899",
+                                             "preservation",
+                                             "99350592312205899-00000002.tif"))
+            ],
+            any_order=True
+        )
+
+
+class TestEASBuilder:
+    @pytest.fixture()
+    def batch_dir(self, monkeypatch):
+
+        # access/
+        #   99338384012205899-00000001.tif
+        #   99338384012205899-00000002.tif
+        #   99338384012205899-00000003.tif
+        #   99350592312205899-00000001.tif
+        #   99350592312205899-00000002.tif
+
+        root = os.path.join("fake", "path")
+        valid_paths = [
+            root,
+            os.path.join(root, "access")
+        ]
+
+        monkeypatch.setattr(
+            eas.os.path,
+            "exists",
+            lambda path: path in valid_paths
+        )
+
+        access_files = [
+            "99338384012205899-00000001.tif",
+            "99338384012205899-00000002.tif",
+            "99338384012205899-00000003.tif",
+            "99350592312205899-00000001.tif",
+            "99350592312205899-00000002.tif",
+        ]
+
+        def scandir(path):
+            for f in access_files:
+                new_file = Mock(
+                    path=os.path.join(path, f),
+                )
+                new_file.name = f
+                yield new_file
+
+        monkeypatch.setattr(eas.os, "scandir", scandir)
+
+        def locate_package_files(_, path: str):
+            for f in access_files:
+                yield pathlib.Path(os.path.join(path, f))
+
+        monkeypatch.setattr(
+            eas.EASBuilder,
+            "locate_package_files",
+            locate_package_files
+        )
+
+        return root
+
+    @pytest.fixture()
+    def batch(self, batch_dir):
+        builder = eas.EASBuilder()
+        new_batch = Batch(batch_dir)
+        builder.build_package(parent=new_batch, path=batch_dir)
+        return new_batch
+
+    def test_package_built(self, batch):
+        assert len(batch.children[0]) == 2
+
+    def test_object_has_items(self, batch):
+        package_object = batch.children[0].children[0]
+        assert len(package_object) == 3
+
+    def test_item_instances(self, batch):
+        package_item = batch.children[0].children[0].children[0]
+        assert len(package_item) == 1
+
+    @pytest.mark.parametrize("file_name, is_eas", [
+        ("99338384012205899-00000001.tif", True),
+        ("99338384012205899-00000002.tif", True),
+        ("99350592312205899-00000001.tif", True),
+        ("99350592312205899-00000002.tif", True),
+        ("99350592312205899_00000002.tif", False),
+        ("abc50592312205899-00000002.tif", False),
+        (os.path.join("batch1", "access"), False),
+    ])
+    def test_is_eas_file(self, file_name, is_eas):
+        builder = eas.EASBuilder()
+        assert builder.is_eas_file(pathlib.Path(file_name)) is is_eas
+
+
---
+++ uiucprescon/packager/packages/eas.py
@@ -0,0 +1,103 @@
+import os
+import pathlib
+import re
+import typing
+from typing import Iterable, Iterator
+from collections import defaultdict
+
+from uiucprescon.packager import Metadata
+from uiucprescon.packager.packages.abs_package_builder import AbsPackageBuilder
+from uiucprescon.packager.packages.collection import Package, \
+    AbsPackageComponent, Batch, PackageObject, Item, Instantiation, InstantiationTypes
+from uiucprescon.packager.packages.collection_builder import \
+    AbsCollectionBuilder
+
+__all__ = ['Eas']
+
+
+class Eas(AbsPackageBuilder):
+
+    def locate_packages(self, path: str) -> Iterator[Package]:
+        yield from EASBuilder().build_batch(path)
+
+    def transform(self, package: Package, dest: str) -> None:
+        raise NotImplementedError("Read only")
+
+
+class EASBuilder(AbsCollectionBuilder):
+    grouper_regex = re.compile(
+        r"^(?P<group>([0-9]+))"
+        r"-"
+        r"(?P<part>[0-9]*)"
+        r"(?P<extension>\.tif?)$"
+    )
+
+    def build_batch(self, root: str) -> AbsPackageComponent:
+        new_batch = Batch(root)
+        self.build_package(parent=new_batch, path=root)
+        return new_batch.children[0]
+
+    def build_instance(self, parent, path: str, filename: str, *args,
+                       **kwargs) -> None:
+        new_instance = Instantiation(parent=parent,
+                                     category=InstantiationTypes.ACCESS)
+        new_instance.component_metadata[Metadata.PATH] = path
+        new_instance._files.append(filename)
+
+    def build_package(self, parent: Batch, path: str, *args, **kwargs) -> None:
+        groups = defaultdict(list)
+        access_path = os.path.join(path, "access")
+        if not os.path.exists(access_path):
+            raise FileNotFoundError(f"No access located in {path}")
+
+        for file in self.locate_package_files(access_path):
+            match_result = EASBuilder.grouper_regex.match(file.name)
+            if match_result is None:
+                raise ValueError(
+                    f"{file.name} does not match expected file naming pattern"
+                )
+
+            group = match_result.groupdict()
+            groups[group['group']].append(file)
+
+        new_package = Package(path, parent=parent)
+        for group_name, packages in groups.items():
+            self.build_object(
+                parent=new_package,
+                group_id=group_name,
+                path=path
+            )
+
+    def is_eas_file(self, path: 'os.PathLike[str]') -> bool:
+        path = pathlib.Path(path)
+        if path.is_dir():
+            return False
+        return EASBuilder.grouper_regex.match(path.name) is not None
+
+    def locate_package_files(self, path: str) -> Iterable[pathlib.Path]:
+        for item in typing.cast(
+                Iterable[os.DirEntry],
+                filter(lambda p: self.is_eas_file(pathlib.Path(p.path)),
+                       os.scandir(path))
+        ):
+            yield pathlib.Path(item.path)
+
+    def build_object(self, parent: Package, group_id: str, path: str) -> None:
+        new_object = PackageObject(parent=parent)
+        new_object.component_metadata[Metadata.ID] = group_id
+        new_object.component_metadata[Metadata.PATH] = path
+        access_path = os.path.join(path, "access")
+        for f in filter(
+                lambda item: self.is_eas_file(pathlib.Path(item.path)),
+                os.scandir(access_path)
+        ):
+            regex_result = EASBuilder.grouper_regex.match(f.name)
+            if regex_result is None:
+                raise ValueError("Unknown pattern")
+
+            groups = regex_result.groupdict()
+            if groups['group'] != group_id:
+                continue
+            new_item = Item(parent=new_object)
+            new_item.component_metadata[Metadata.ITEM_NAME] = regex_result['part']
+            self.build_instance(new_item, path=os.path.dirname(f.path), filename=f.name)
