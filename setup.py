5c665a832f526f014a915d585f3c8fb2e9bfb3c6
Henry Borchers <hborcher@illinois.edu>
2021-09-09 11:49:40 -0500

    support pep-561 typed packages (#67)

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     test_suite="tests",
     setup_requires=['pytest-runner'],
     install_requires=['py3exiv2bind>=0.1.5'],
+    package_data={"uiucprescon.packager": ["py.typed"]},
     extras_require={"kdu": ['pykdu-compress>=0.1.3']},
     tests_require=['pytest', 'pytest-bdd<4.0'],
     zip_safe=False,
