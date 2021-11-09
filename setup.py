d8905c1beee08a97128a6942799f3770e4e2273a
henry <henryborchers@yahoo.com>
2021-11-09 14:41:11 -0600

    install_require 'py3exiv2bind>=0.1.9b1'

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     namespace_packages=["uiucprescon"],
     test_suite="tests",
     setup_requires=['pytest-runner'],
-    install_requires=['py3exiv2bind>=0.1.5'],
+    install_requires=['py3exiv2bind>=0.1.9b1'],
     package_data={"uiucprescon.packager": ["py.typed"]},
     extras_require={"kdu": ['pykdu-compress>=0.1.7b2']},
     tests_require=['pytest', 'pytest-bdd<4.0'],
