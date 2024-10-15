24e8c5cd5560178c63dc6566aa2e03d60c57d1cd
Henry Borchers <henryborchers@yahoo.com>
2024-10-15 13:01:18 -0500

    build: move project metadata from setup.cfg & setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
 from setuptools import setup
 
-setup(
-    packages=[
-        'uiucprescon.packager',
-        'uiucprescon.packager.packages',
-    ],
-    test_suite="tests",
-    install_requires=['py3exiv2bind>=0.1.9'],
-    package_data={"uiucprescon.packager": ["py.typed"]},
-    extras_require={"kdu": ['pykdu-compress>=0.1.8']},
-    tests_require=['pytest', 'pytest-bdd<4.0'],
-    zip_safe=False,
-    python_requires='>=3.8',
-)
+setup()
